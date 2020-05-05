b2c40fac7ca929cb96f0246906c1783d36296bfa
Artem Ryabov <artem.r@tonlabs.io>
2020-05-05 18:03:12 +0300

    Updated multisig validator

--- scripts/msig_genaddr.sh
+++ scripts/msig_genaddr.sh
@@ -19,7 +19,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 # shellcheck source=env.sh
 . "${SCRIPT_DIR}/env.sh"
 
-"${TON_BUILD_DIR}/utils/tonos-cli" genaddr "${NET_TON_DEV_SRC_TOP_DIR}/configs/MultisigWallet.tvc" \
-    "${NET_TON_DEV_SRC_TOP_DIR}/configs/MultisigWallet.abi.json" --genkey "${KEYS_DIR}/msig.keys.json" --wc -1 |
+"${TON_BUILD_DIR}/utils/tonos-cli" genaddr "${NET_TON_DEV_SRC_TOP_DIR}/configs/SafeMultisigWallet.tvc" \
+    "${NET_TON_DEV_SRC_TOP_DIR}/configs/SafeMultisigWallet.abi.json" --genkey "${KEYS_DIR}/msig.keys.json" --wc -1 |
     grep "Raw address" |
     cut -d ' ' -f 3 >"${KEYS_DIR}/${VALIDATOR_NAME}.addr"
